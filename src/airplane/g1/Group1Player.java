23c4c5bc616f5e8950c822be89213c2989155735
Skyler Cheung <skcheung@seas.upenn.edu>
2024-07-10 22:51:31 -0400

    method to check for intersection

--- src/airplane/g1/Group1Player.java
+++ src/airplane/g1/Group1Player.java
@@ -35,7 +35,6 @@ public class Group1Player extends Player {
         double bearing1 = 0;
         double bearing2 = 0;
 
-
         // Check if the plane is in the air
         if (round >= departureTime1) {
             bearing1 = calculateBearing(p1.getLocation(), p1.getDestination());
@@ -95,5 +94,40 @@ public class Group1Player extends Player {
         double xdist = x1 - x2;
         double ydist = y1 - y2;
         return Math.sqrt(xdist * xdist + ydist * ydist);
+    }
+
+    // determines whether two line segments given by two pairs of coordinates would intersect
+    // returns point of intersection if so; otherwise, empty double[]
+    private double[] intersects(double ax, double ay, double bx, double by,
+                              double cx, double cy, double dx, double dy) {
+        double a1 = by - ay;
+        double b1 = ax - bx;
+        double c1 = a1 * ax + b1 * ay;
+
+        double a2 = dy - cy;
+        double b2 = cx - dx;
+        double c2 = a2 * cx + b2 * cy;
+
+        double determinant = a1 * b2 - a2 * b1;
+
+        if (determinant == 0) {
+            return new double[0];
+        } else {
+            double x = (b2 * c1 - b1 * c2) / determinant;
+            double y = (a1 * c2 - a2 * c1) / determinant;
+
+            // checks whether intersection is on both line segments
+            if (Math.min(ax, bx) <= x && x <= Math.max(ax, bx) &&
+                    Math.min(ay, by) <= y && y <= Math.max(ay, by) &&
+                    Math.min(cx, dx) <= x && x <= Math.max(cx, dx) &&
+                    Math.min(cy, dy) <= y && y <= Math.max(cy, dy)) {
+                double[] point = new double[2];
+                point[0] = x;
+                point[1] = y;
+                return point;
+            } else {
+                return new double[0];
+            }
+        }
     }
 }
